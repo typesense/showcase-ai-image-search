9d148c6a42d7bb52ab9ca1bb5d6fe88723790d07
Phi Hung Hoang <hphihung795@gmail.com>
2024-03-26 23:34:36 +0700

    open modal when click on image
    Former-commit-id: 7ef13f326c6f72c8025fa64961cef94c21a3a950

--- src/components/ImageSearch.tsx
+++ src/components/ImageSearch.tsx
@@ -1,44 +1,56 @@
 'use client';
 import useImageSearch from '@/hooks/useImageSearch';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { Masonry } from 'react-plock';
+import Modal from './Modal';
 
 export default function ImageSearch() {
   const { hits, nextPage } = useImageSearch();
   const { ref, inView } = useInView({ threshold: 0.001 });
-
+  const [activeHit, setActiveHit] = useState(null);
   useEffect(() => {
     if (inView) nextPage();
   }, [inView]);
 
   return (
-    <div className='w-full'>
-      <Masonry
-        className='min-h-screen'
-        items={hits}
-        config={{
-          columns: [2, 4, 6],
-          gap: [6, 6, 6],
-          media: [640, 768, 1024],
-        }}
-        render={({ document }: any) => (
-          <div key={document.id} onClick={() => console.log(document)}>
-            <Image
-              src={`/part-1-2-2m/${document.id}`}
-              width={0}
-              height={0}
-              sizes='20vw'
-              style={{ width: '100%', height: 'auto' }}
-              alt={document.prompt}
-            />
-            <span>{document.prompt}</span>
-          </div>
-        )}
-      />
-      <div ref={ref} className='h-[10vh] mt-[-120vh]'></div>
-    </div>
+    <>
+      <section className='w-full'>
+        <Masonry
+          className='min-h-screen'
+          items={hits}
+          config={{
+            columns: [2, 4, 6],
+            gap: [6, 6, 6],
+            media: [640, 768, 1024],
+          }}
+          render={({ document }: any) => (
+            <li
+              className='cursor-pointer list-none'
+              onClick={() => setActiveHit(document)}
+              key={document.id}
+            >
+              <Image
+                src={`/part-1-2-2m/${document.id}`}
+                width={0}
+                height={0}
+                sizes='20vw'
+                style={{ width: '100%', height: 'auto' }}
+                alt={document.prompt}
+              />
+              <span>{document.prompt}</span>
+            </li>
+          )}
+        />
+        <div ref={ref} className='h-[10vh] mt-[-120vh]'></div>
+      </section>
+
+      {activeHit && (
+        <Modal data={activeHit} handleClose={() => setActiveHit(null)} />
+      )}
+    </>
   );
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+const SAMPLER = {
+  1: 'ddim',
+  2: 'plms',
+  3: 'k_euler',
+  4: 'k_euler_ancestral',
+  5: 'k_heun',
+  6: 'k_dpm_2',
+  7: 'k_dpm_2_ancestral',
+  8: 'k_lms',
+  9: 'others',
+};
+export default function Modal({ data, handleClose }: any) {
+  return (
+    <section className='fixed inset-0 grid place-items-center'>
+      <div className='flex relative z-10 w-[80vw] h-[90vh] gap-10 bg-slate-950 rounded-xl'>
+        <div className='relative w-[70%] h-full grid place-items-center rounded-xl bg-black'>
+          <Image
+            className='w-full h-full max-h-[90vh] object-contain'
+            src={`/part-1-2-2m/${data.id}`}
+            width={0}
+            height={0}
+            sizes='80vw'
+            alt={data.prompt}
+          />
+        </div>
+        <div className='flex-1'>
+          <span>Prompt</span>
+          <p>{data.prompt}</p>
+          <span>Seed</span>
+          <p>{data.seed}</p>
+          <span>CFG scale</span>
+          <p>{data.cfg}</p>
+          <span>Steps</span>
+          <p>{data.step}</p>
+          <span>Sampler</span>
+          <p>{SAMPLER[data.sampler]}</p>
+        </div>
+      </div>
+      <div
+        className='fixed inset-0 z-0 bg-slate-950 opacity-70'
+        onClick={handleClose}
+      ></div>
+    </section>
+  );
+}
