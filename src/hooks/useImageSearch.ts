8bea6d4892f83a35b1abfa29aa62bb8db0d3acb8
Phi Hung Hoang <hphihung795@gmail.com>
2024-03-30 21:29:56 +0700

    feat: search images with text
    Former-commit-id: a2047cbdc8a1e15047147118bc545b779d72af14

--- src/hooks/useImageSearch.ts
+++ src/hooks/useImageSearch.ts
@@ -2,6 +2,7 @@ import { typesense } from '@/lib/typesense';
 import { useImageSearchParams } from '@/types/useImageSearch';
 import { random } from '@/utils/random';
 import { useEffect, useState } from 'react';
+import { SearchParams } from 'typesense/lib/Typesense/Documents';
 
 // simulate randomness
 let randomPage = random(0, 80);
@@ -10,30 +11,43 @@ export default function useImageSearch({
   enableRandomPage,
   ...searchParameters
 }: useImageSearchParams) {
+  const [page, setPage] = useState(enableRandomPage ? randomPage : 1);
   const [hits, setHits] = useState<any>([]);
+  const [searchBoxParams, setSearchBoxParams] = useState<SearchParams | null>(
+    null,
+  );
 
   useEffect(() => {
-    if (!enableRandomPage) randomPage = 0;
     nextPage();
   }, []);
 
-  const nextPage = async () => {
-    if (randomPage >= 80 && enableRandomPage) randomPage = 0;
-    randomPage++;
+  const nextPage = async (customSearchParams?: SearchParams | null) => {
     try {
       const res = await typesense
         .collections('DiffusionDB')
         .documents()
         .search({
-          ...searchParameters,
-          page: randomPage,
+          ...(customSearchParams || searchParameters),
+          page,
         });
       console.log(res);
+      setPage((prev) => (prev > 79 && enableRandomPage ? 1 : prev + 1));
 
       setHits((prev: any) => [...prev, ...(res.hits || [])]);
     } catch (error) {
       alert('Sorry, there is an error fetching data!');
     }
   };
-  return { hits, nextPage };
+
+  const search = (searchParams: SearchParams) =>
+    setSearchBoxParams(searchParams);
+
+  useEffect(() => {
+    if (!searchBoxParams?.q) return;
+    setHits([]);
+    setPage(0);
+    nextPage(searchBoxParams);
+  }, [searchBoxParams?.q]);
+
+  return { hits, nextPage, search };
 }
