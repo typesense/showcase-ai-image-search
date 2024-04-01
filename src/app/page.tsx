86076d4990aa285f19f123e65bb9a90116c75bdb
Phi Hung Hoang <hphihung795@gmail.com>
2024-04-01 19:46:26 +0700

    perform search on random popular keywords
    Former-commit-id: 0eb8d672c829097fd8a1365621f8b06be1064c28

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 'use client';
 import ImageSearch from '@/components/ImageSearch';
+import { random } from '@/utils/random';
 import { useSearchParams } from 'next/navigation';
+const popularKeywords = [
+  'dog',
+  'cat',
+  'tree',
+  'fire',
+  'ice',
+  'robot',
+  'people',
+  'ocean',
+  'orange',
+];
+const idx = random(0, popularKeywords.length - 1);
 
 export default function Home() {
   const searchParams = useSearchParams();
-  const query = searchParams.get('q') || '*';
+  const query = searchParams.get('q') || popularKeywords[idx];
   return (
     <>
       <div>
