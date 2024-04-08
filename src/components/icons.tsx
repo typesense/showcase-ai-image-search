42bcb14570e897fa747d9327015f04f3e7941648
Phi Hung Hoang <hphihung795@gmail.com>
2024-04-08 19:52:32 +0700

    fix: invalid svg DOM property

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -20,10 +20,10 @@ export function ChevronRight({ ...svgProps }: _svgProp) {
     <svg
       stroke='currentColor'
       fill='none'
-      stroke-width='2'
+      strokeWidth='2'
       viewBox='0 0 24 24'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       height='1em'
       width='1em'
       xmlns='http://www.w3.org/2000/svg'
