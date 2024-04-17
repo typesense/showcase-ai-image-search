07ce1349db6dc6f494f1e286ae7fe51ebe35e754
Phi Hung Hoang <hphihung795@gmail.com>
2024-04-17 20:12:59 +0700

    🎨: add search icon

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -3,6 +3,7 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { EXAMPLE_SEARCH_TERMS } from '@/utils/CONSTANTS';
+import { SearchIcon } from './icons';
 
 /*
  * This component contain a search box and a form that when submitted will push the route to /?q={searchBoxValue}
@@ -30,13 +31,16 @@ export default function SearchBox({
           router.push(`/?q=${query}`);
         }}
       >
-        <input
-          className='h-[5vmax] max-h-12 min-h-10 w-[max(50vw,350px)] max-w-[95vw] rounded-xl border-2 border-dark-500 bg-dark-500 px-6 py-2 text-sm shadow-2xl shadow-[black] placeholder:opacity-50 focus:border-accent focus:border-opacity-40 focus:outline-none'
-          type='text'
-          placeholder='Search for images...'
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <div className='relative'>
+          <SearchIcon className='absolute top-[50%] ml-4 translate-y-[-50%] fill-white-400' />
+          <input
+            className='h-[5vmax] max-h-12 min-h-10 w-[max(50vw,350px)] max-w-[95vw] rounded-xl border-2 border-dark-500 bg-dark-500 py-2 pl-10 pr-4 text-sm shadow-2xl shadow-[black] placeholder:text-white-400 placeholder:opacity-50 focus:border-accent focus:border-opacity-40 focus:outline-none'
+            type='text'
+            placeholder='Search for images...'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
       </form>
       {showExampleSearchTerms && (
         <ul className='m-auto mt-2 flex w-[max(50vw,350px)] max-w-[95vw] gap-1 overflow-x-auto font-mono text-xs text-white-400'>
